b33f27b098c25ed282612e7298b7d2741c607292
felipepsaraiva <felipepsaraiva@users.noreply.github.com>
2023-05-09 15:07:43 -0300

    Fix package version convention

--- export.sh
+++ export.sh
@@ -1,3 +1,3 @@
-version=$(cat package.xml | grep "<version>" | sed 's|\ *<\/*version>\ *||g')
+version=$(cat package.xml | grep "<webrtc_version>" | sed 's|\ *<\/*webrtc_version>\ *||g')
 mkdir -p output
 docker run --rm -v ./output:/output ${1:-synkar_webrtc_build} /bin/bash -c "tar -czvf /output/webrtc-$version.tar.gz -C /opt webrtc"
